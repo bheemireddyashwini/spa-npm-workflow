e195bf65daa4c8fdb70f3de24071ace6c4944d20
Ashwini-Dci-student <ashwini.bheemireddy@dci-student.org>
2024-09-19 23:29:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import ora from "ora";
+import chalk from "chalk";
+import getNextColor from "./functions.js";
+getNextColor();
+
+const spinner = ora(`Loading rainbows`).start();
+
+setInterval(() => {
+  const color = getNextColor();
+  spinner.color = color;
+  spinner.text = `Loading ${chalk[color]("rainbows")}`;
+}, 500);
+
+setTimeout(() => {
+  spinner.stop();
+  process.exit();
+}, 10000);
+
+
